441462a75456518627d8aae9f59a58c0abd24d50
Isa <isa0021@mail.ru>
2022-06-14 10:10:41 +0300

    поиск врачей

--- src/components/pages/DocPage/Content/Content.jsx
+++ src/components/pages/DocPage/Content/Content.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import style from "./Content.module.css";
 
-function Content() {
+function Content({ doctor }) {
   return (
-    <div>Content</div>
-  )
+    <div className={style.cart}>
+      <div>
+        {" "}
+        <img src={doctor.photo} alt="" />
+      </div>
+      <hr />
+      <div>{doctor.name}</div>
+      <div>{doctor.lastName}</div>
+      <div>Рейтинг:{doctor.rating}</div>
+      <div>Место работы :{doctor.place}</div>
+    </div>
+  );
 }
 
-export default Content
+export default Content;
--- src/components/pages/DocPage/DocPage.jsx
+++ src/components/pages/DocPage/DocPage.jsx
@@ -2,17 +2,25 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getCategory } from "../../../redux-toolkit/features/categories";
 import { getDoctors } from "../../../redux-toolkit/features/doctorSlice";
+import Content from "./Content/Content";
 import Sidebar from "./Sidebar/Sidebar";
-
+import style from "./Docpage.module.css";
 const DocPage = (id) => {
   const doctors = useSelector((state) => state.doctor.doctors);
 
   const [filtered, setFiltered] = useState(doctors);
-
+  const [value, setValue] = useState("");
   const dispatch = useDispatch();
 
   const categories = useSelector((state) => state.categories.categories);
   const loading = useSelector((state) => state.doctor.loading);
+
+  const docs = doctors.filter((doc) => {
+    return (
+      doc.name.toLowerCase().includes(value.toLowerCase()) ||
+      doc.lastName.toLowerCase().includes(value.toLowerCase())
+    );
+  });
 
   useEffect(() => {
     dispatch(getCategory());
@@ -24,8 +32,13 @@ const DocPage = (id) => {
   };
 
   return (
-    <div>
+    <div className={style.content}>
       <div>
+        <input
+          type="text"
+          placeholder="Начните поиск врача"
+          onChange={(event) => setValue(event.target.value)}
+        />
         <div onClick={(e) => setFiltered(doctors)}>Все категории</div>
         {categories.map((category) => {
           return (
@@ -38,10 +51,12 @@ const DocPage = (id) => {
         })}
       </div>
 
-      <h1>Врачи</h1>
-      {filtered.map((doctor, index) => {
-        return <div key={index}>{doctor.name}</div>;
-      })}
+      <div className={style.content_carts}>
+        <h1>Врачи</h1>
+        {filtered.map((doctor, index) => {
+          return <Content doctor={doctor} key={index}></Content>;
+        })}
+      </div>
     </div>
   );
 };

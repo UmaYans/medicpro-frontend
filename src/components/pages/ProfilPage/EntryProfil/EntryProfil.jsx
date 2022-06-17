258af40a2f4601ca910985614190ea31bc20c5db
Denu <Denu@dota2.ru>
2022-06-17 11:59:41 +0300

    kek

--- src/components/pages/ProfilPage/EntryProfil/EntryProfil.jsx
+++ src/components/pages/ProfilPage/EntryProfil/EntryProfil.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getEntryDocId } from "../../../../redux-toolkit/features/entrySlice"
 
 const EntryProfil = () => {
+
+    const entry = useSelector((state) => state.entry.entry);
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getEntryDocId());
+      }, [dispatch]);
+
     return (
         <div>
-            EntryProfil
+            {entry.time}
+            <div>
+            {entry.time}
+        </div>
         </div>
     );
 };

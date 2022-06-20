ced2c2e835fb1e12123003a1b3e564db9be7a716
Denu <Denu@dota2.ru>
2022-06-20 14:30:16 +0300

    end

--- src/components/pages/ProfilPage/PersonalPage.jsx
+++ src/components/pages/ProfilPage/PersonalPage.jsx
@@ -16,23 +16,6 @@ const ProfilPage = () => {
         <div className={styles.content} >
           <Outlet />
         </div>
-
-        {/* <div className={styles.flex} onClick={prof}>мой профиль {get ? <div>
-        <div>{users.name}</div>
-        <div>{users.lastName}</div>
-        <div>{users.telephone}</div>
-        </div> : null }</div>
-        <div onClick={prof2}>комментарии<div>{get1 ?  <div>
-        {comments.map((item) => {
-          return (
-            <div key={item._id}>
-              <div>{item.text}</div>
-              <button onClick={() => deleteTodo(item._id)}>x</button>
-            </div>
-          );
-        })}
-      </div> : null}</div></div>
-        <li>услуги</li> */}
       </div>
       <div></div>
     </div>
